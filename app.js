0648a68c60063feafc8b1b17bdd9db04a2d90edc
pavan <parvathaneniteja9@gmail.com>
2026-07-20 15:18:43 -0500

    Added new files for GCP deployment setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Cloud Run!');
+  res.send('Hello World from Cloud Run & GKE!');
 });
 
 const port = process.env.PORT || 8080;
